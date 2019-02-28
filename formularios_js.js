e7d26d4b1bd6be3247abca10628f315222696f24
JoselynBartloRubio <46981139+JoselynBartloRubio@users.noreply.github.com>
2019-02-28 12:37:35 -0600

    actualizacion curriculum

---
+++ formularios_js.js
@@ -0,0 +1,40 @@
+Zepto (function ($){
+
+let costo = 0;
+let masas = {
+cl: 50,
+cr: 75,
+li:45,
+};
+let sizes = {
+    t_chica: 50,
+    t_mediana: 70,
+    t_grande: 90,
+};
+const ppi = 5;//precio por ingrediente
+const ppie = 7;//precio por ingrediente extra
+
+/*//obtencion de los elementos del formmulario
+ let masa = $(masas);
+ console.log(masa.val());
+
+
+ let masa = $("#masa");
+let size = $("input[name=tamano]").filter(checked)
+console.log(size);
+
+ // calculo de costosz
+ costo += masas[masa.val()];
+ console.log(costo);*/
+let masa = &("masa")
+
+
+});
+
+let checked = function () { return
+this.checked;
+}
+
+let selected = function () {return
+    this.selected;
+}
